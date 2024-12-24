4d428275f3c74b3018917aa72b735350e464124b
Junior <68521496+jrftw@users.noreply.github.com>
2024-12-23 23:15:08 -0500

    Initial Commit

---
+++ Avoid The TT BanTests/Avoid_The_TT_BanTests.swift
@@ -0,0 +1,17 @@
+//
+//  Avoid_The_TT_BanTests.swift
+//  Avoid The TT BanTests
+//
+//  Created by Kevin Doyle Jr. on 12/23/24.
+//
+
+import Testing
+@testable import Avoid_The_TT_Ban
+
+struct Avoid_The_TT_BanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
